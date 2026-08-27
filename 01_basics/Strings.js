7a1ec7d27f71cd109e6628d2f6eb140975b78d21
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-27 11:03:01 +0000

    Strings Done

---
+++ 01_basics/Strings.js
@@ -0,0 +1,47 @@
+let Name= "Afaq"
+
+let repoCount= 30
+
+
+// console.log("My name is "+ Name+" my repo counts is"+repoCount)
+
+
+console.log(`My name is ${Name} and my repo counts is ${repoCount}`)
+
+
+let gameName= new String("The game name is Vice City.")
+
+console.log(gameName)
+
+console.log(String.fromCharCode(10, 20, 30, 40));
+console.log(String.fromCodePoint(9731, 9733, 9842, 0x2f804));
+
+
+console.log(gameName.at(6))
+console.log(gameName.big())
+console.log(gameName.blink())
+console.log(gameName.bold())
+console.log(gameName.charAt(0))
+console.log(gameName.charCodeAt(5))
+console.log(gameName.codePointAt(5))
+console.log(gameName.concat(" And i love this game."))
+console.log(gameName.endsWith("City."))
+console.log(gameName.fixed())
+console.log(gameName.length)
+console.log(gameName.fontcolor("green"))
+console.log(gameName.fontsize("24"))
+console.log(gameName.includes("City"))
+console.log(gameName.indexOf("City"))
+console.log(gameName.isWellFormed())
+console.log(gameName.italics())
+console.log(gameName.lastIndexOf("e"))
+console.log(gameName.match("game"))
+console.log(gameName.matchAll("game"))
+console.log(gameName.normalize())
+console.log(gameName.padEnd(5))
+console.log(gameName.padStart("a"))
+console.log(gameName.repeat(3))
+console.log(gameName.replace("Vice City.","GTA."))
+console.log(gameName.search("a"))
+console.log(gameName.slice(4,15))
+console.log(gameName.split(" "))
