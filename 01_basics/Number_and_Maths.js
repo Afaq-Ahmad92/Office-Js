c0f505fc354fa35aa9e71e1f84c22db091102488
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-27 11:49:22 +0000

    Number and Math

---
+++ 01_basics/Number_and_Maths.js
@@ -0,0 +1,29 @@
+let Score = new Number(200000)
+
+
+// console.log(Score.toString())
+// console.log(Score.toString().length)
+// console.log(Score.toFixed(1))
+// console.log(Score.toPrecision(5))
+// console.log(Score.toExponential(6))
+// console.log(Score.toLocaleString())
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
+console.log(Math.abs(-5))
+console.log(Math.round(4.6))
+console.log(Math.ceil(5.1))
+console.log(Math.floor(5.1))
+console.log(Math.min(2,4,5,1))
+console.log(Math.max(222,444,500,100))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor((Math.random()*10)+1))
+
+const Min =10
+const Max= 20
+
+console.log(Math.floor(Math.random()*(Max-Min+1))+Min)
