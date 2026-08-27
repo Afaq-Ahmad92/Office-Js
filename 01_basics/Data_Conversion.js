1f43182edbf746ea06ce011f0afd838365fcaba6
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-27 07:27:44 +0000

    Conversion

---
+++ 01_basics/Data_Conversion.js
@@ -0,0 +1,45 @@
+let Age= "31"
+
+console.log(Age)
+console.log(typeof(Age))
+
+let AgeInNumber = Number(Age)
+
+console.log(AgeInNumber)
+console.log(typeof AgeInNumber)
+
+Age="31sasdasd"
+
+console.log(Age)
+console.log(typeof(Age))
+
+AgeInNumber = Number(Age)
+
+console.log(AgeInNumber)
+console.log(typeof AgeInNumber)
+
+
+// let Status = "1"
+// let Status = 1
+// let Status = ""
+   let Status = "0"
+
+console.log(typeof Status)
+console.log(Status)
+
+let StatusNum= Boolean(Status)
+
+console.log(typeof StatusNum)
+console.log(StatusNum)
+
+
+
+let Value= 20
+
+console.log(typeof Value)
+console.log( Value)
+
+let StringValue= String (Value)
+
+console.log(typeof StringValue)
+console.log(StringValue)
