c7bd2b87c776c3de646151336b3d4938194f3b1d
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-27 06:12:15 +0000

    Variables Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 34242
+let accountEmail="test@gmail.com"
+var accountPassword="121212"
+cityName="Peshawer"
+let accountStatus
+
+
+// accountId = 4242 this is not allowed 
+
+accountEmail= "work@gmail.com"
+accountPassword= "212121"
+cityName= 'Charsadda'
+
+console.table([accountId, accountEmail, accountPassword, cityName, accountStatus])
