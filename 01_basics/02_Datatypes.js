a733ea4bcba200b3542024debffd6b394bd52fcc
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-27 06:32:14 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let Name ="Afaq"
+let Age= 24
+let status= true
+let temprature = null
+let Married
+
+
+console.log(typeof Name)
+console.log(typeof Age)
+console.log(typeof status)
+console.log(typeof temprature)
+console.log(typeof Married)
