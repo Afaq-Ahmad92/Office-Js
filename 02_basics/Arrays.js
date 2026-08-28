47cec78251452e13972d5bc148f254cc4b60b717
Muhammad Afaq Ahmad <146156785+Afaq-Ahmad92@users.noreply.github.com>
2026-08-28 07:16:12 +0000

    Array First Part Done

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,28 @@
+// let myArray= [1,2,33,422,223]
+
+// console.log(myArray[3])
+
+let newArray= new Array(21,3,1,3,4,5)
+// newArray.push(21)
+// newArray.push(122)
+
+// console.log(newArray)
+// newArray.pop()
+
+// console.log(newArray)
+
+// newArray.unshift(0)
+// console.log(newArray)
+// newArray.shift()
+// console.log(newArray)
+
+// console.log(newArray.length)
+
+// console.log(newArray.includes(0))
+// console.log(newArray.indexOf(4))
+
+console.log(newArray.slice(2,5))
+console.log(newArray)
+
+console.log(newArray.splice(2,5))
+console.log(newArray)
