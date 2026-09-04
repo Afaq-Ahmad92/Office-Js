64277d2e8de92cb04f080457730725db568a6803
Muhammad Afaq Ahmad <eengr2927@gmail.com>
2026-09-04 11:23:45 +0400

    Scopes Done

---
+++ 03_basics/Scopes.jsx
@@ -0,0 +1,60 @@
+// let a =10
+// const b= 20
+// var c= 30
+
+
+// if (true) {
+//     let a = 100
+//     const b = 200
+//     var c = 300
+//     console.log("a :", a)
+//     console.log("b :", b)
+//     console.log("c :", c)
+// }
+// console.log("a :", a)
+// console.log("b :", b)
+// console.log("c :", c)
+
+// function addvalue(){
+//     let a= 30
+
+//     console.log("First Fun a :",a)
+    
+//     function addnumb(){
+//         let b = 300
+//         console.log("Second Fun a :",a)
+//         console.log("b :",b)
+
+//     }
+// // console.log("B: ",b)
+// addnumb()
+// }
+
+// addvalue()
+
+if(true){
+    const name ="Afaq"
+    if(name === "Afaq"){
+        const friend= "Hussain"
+        console.log(`My name is ${name} and friend name is ${friend}`);
+    }
+    // console.log(`My frined name is : ${friend}`)
+}
+
+// console.log(`My name is ${name}`)
+
+console.log(name("Afaq"));
+
+
+function name(Name){
+    return Name
+}
+
+
+
+// console.log(friend("Hussain"));
+
+
+const friend = function(value){
+    return value
+}
